9f72b6531758e6488e230ba5f39df4da5936da8d
Dodge <toddbutler93@outlook.com.au>
2024-09-07 18:00:25 +1000

    Update taserver installer for latest master branch commit

--- loginserver/install_taserver.sh
+++ loginserver/install_taserver.sh
@@ -3,11 +3,9 @@
 # This script installs taserver during docker build
 set -ex
 
-TASERVER_RELEASE_TAG="v2.8.0" 
-
 # Get taserver
 if [ ! -f taserver.zip ]; then
-  wget -q -O taserver.zip "https://github.com/Griffon26/taserver/archive/refs/tags/$TASERVER_RELEASE_TAG.zip"
+  wget -q -O taserver.zip "https://github.com/Griffon26/taserver/archive/refs/heads/master.zip"
   unzip -q taserver.zip
   mv "$(ls | grep taserver-*)" "taserver" 
 else
